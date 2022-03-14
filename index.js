0340de5a0e003cd2332b5bf280cbc178e70d8eb4
Lucky-Tsuma <tsumahmumba@gmail.com>
2022-03-14 16:34:55 +0300

    Toogle navigation bar for smaller screens

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ const plans = [
 ];
 
 const divPlans = document.getElementById('plans');
+let navBarDisplay = document.getElementById('nav-bar').style.display;
+const menuIcon = document.getElementById('menu-icon');
+const header = document.getElementById('header');
+const sizer = document.getElementById('sizer');
+const links = document.querySelectorAll('.nav-link');
 
 plans.map(element => {
     divPlans.innerHTML += `<div class="plan-item">
@@ -33,4 +38,44 @@ plans.map(element => {
                     <button class="btn-select">SELECT</button>
                 </div>
             </div>`
+});
+
+menuIcon.onclick = () => {
+    if (navBarDisplay === 'none') {
+        document.querySelector('#nav-bar').style.display = 'flex';
+        header.style.height = '340px';
+        sizer.style.minHeight = '340px';
+        navBarDisplay = 'flex';
+    } else {
+        document.querySelector('#nav-bar').style.display = "none";
+        header.style.height = '140px';
+        sizer.style.minHeight = '140px';
+        navBarDisplay = 'none';
+    }
+}
+
+for(let i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', function(){
+    if(window.innerWidth < 720) {
+        document.querySelector('#nav-bar').style.display = "none";
+        header.style.height = '140px';
+        sizer.style.minHeight = '140px';
+        navBarDisplay = 'none';
+    }
+  });
+}
+
+
+window.addEventListener('resize', function(){
+    if(this.window.innerWidth > 720) {
+        document.querySelector('#nav-bar').style.display = 'flex';
+        navBarDisplay = 'flex';
+        header.style.height = '80px';
+        sizer.style.minHeight = '80px';
+    } else {
+        document.querySelector('#nav-bar').style.display = 'none';
+        navBarDisplay = 'none';
+        header.style.height = '140px';
+        sizer.style.minHeight = '140px';
+    }
 });
